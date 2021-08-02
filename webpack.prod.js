97deab582b3cfef93ded94c0707b340ede070888
labsan <yondeveloper@gmail.com>
2021-08-02 22:08:51 +0700

    Add bundle optimization & code splitting

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,33 @@
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: { // New object for Bundle Optimization & Code Splitting
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000, // Minimal size bundle
+      maxSize: 70000, // Maximal size bundle
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendor: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {
@@ -20,5 +44,12 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+      generateStatsFile: true,
+      statsOptions: {source: false},
+    }),
+  ],
 });
