5947670f2a5129e8f052680df83851d643efb384
labsan <yondeveloper@gmail.com>
2021-08-04 14:55:42 +0700

    🔨 Update code splitting implementation

--- src/scripts/utils/swal-initiator.js
+++ src/scripts/utils/swal-initiator.js
@@ -1,21 +1,31 @@
-import Swal from 'sweetalert2';
+// implementing code splitting
 
 const INIT_SWAL_SUCCESS = (title) => {
-  Swal.fire({
-    title,
-    toast: true,
-    icon: 'success',
-    confirmButtonText: 'Ok',
-  });
+  import('sweetalert2')
+      .then((module) => module.default)
+      .then((swal) => {
+        swal.fire({
+          title,
+          toast: true,
+          icon: 'success',
+          confirmButtonText: 'Ok',
+        });
+      })
+      .catch((err) => console.error(err));
 };
 
 const INIT_SWAL_ERROR = (title) => {
-  Swal.fire({
-    title,
-    toast: true,
-    icon: 'error',
-    confirmButtonText: 'Ok',
-  });
+  import('sweetalert2')
+      .then((module) => module.default)
+      .then((swal) => {
+        swal.fire({
+          title,
+          toast: true,
+          icon: 'error',
+          confirmButtonText: 'Ok',
+        });
+      })
+      .catch((err) => console.error(err));
 };
 
 export {INIT_SWAL_SUCCESS, INIT_SWAL_ERROR};
