9927918074b467c0e2cb06db307999712113ff5a
labsan <yondeveloper@gmail.com>
2021-08-06 13:35:31 +0700

    🐛 Fix bug e2e testing feature

--- e2e/Favorite_Restaurant.spec.js
+++ e2e/Favorite_Restaurant.spec.js
@@ -1,62 +1,83 @@
 const assert = require('assert');
-
 Feature('Favorite Restaurants');
 
 Before(({I}) => {
-  // START_URL: localhost:9191/#/favorite
-  I.amOnPage('/#/favorite');
+  I.amOnPage('/#/favorite');  // START_URL: localhost:9191/#/favorite
 });
 
-// Value variabel ini menyesuaikan kalimat pada halaman Favorite saat tidak memiliki data restoran dihalamannya
-const emptyFavoriteRestoText = 'Restoran yang di-favoritkan tidak ada. Klik button favorit pada halaman informasi restoran.';
+/*
+  Value variabel ini menyesuaikan kalimat yang ditampilkan pada halaman Favorite
+  ketika tidak memiliki data restoran dihalamannya
+*/ 
+const emptyFavoriteRestoText = 'Tidak ada galeri restoran yang ditampilkan.';
 
 // SKENARIO UJI 1
 Scenario('menampilkan halaman favorit restoran yang kosong', ({I}) => {
-  // Saya mencari elemen #fav-resto
   I.seeElement('#fav-resto');
-
-  // Saya melihat teks pada elemen #fav-resto
   I.see(emptyFavoriteRestoText, '#fav-resto');
 });
 
 // SKENARIO UJI 2
 Scenario('menampilkan satu galeri restoran yang di-favoritkan', async ({I}) => {
-  // Pengguna melihat teks pada elemen #fav-resto
   I.see(emptyFavoriteRestoText, '#fav-resto');
 
-    // URL: /
-    I.amOnPage('/');
-    I.seeElement('.card');
-    const FIRST_RESTO_CARD = locate('.card-content-title').first();
-    // const FIRST_RESTO_CARD_TITLE = await I.grabTextFrom(FIRST_RESTO_CARD);
-    I.click(FIRST_RESTO_CARD);
+  // URL: /
+  I.amOnPage('/');
+  I.seeElement('.card');
+  const FIRST_RESTO = locate('.card-content-title').first();
+  const FIRST_RESTO_TITLE = await I.grabTextFrom(FIRST_RESTO);
+  I.click(FIRST_RESTO);
 
-    // URL: /#/:detail
-    I.seeElement('#likeButton');
-    I.click('#likeButton');
-    pause();
+  // URL: /#/:detail
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
+  // URL: /#/favorite
+  I.amOnPage('/#/favorite');
+  I.seeElement('.card');
 
-    // URL: /#/favorite
-    // I.amOnPage('/#/favorite');
-    // I.seeElement('.card');
-    // const likedRestoTitle = await I.grabTextFrom('.card-content-title');
-    // assert.strictEqual(FIRST_RESTO_CARD_TITLE, likedRestoTitle);
+  const FAV_RESTO_TITLE = await I.grabTextFrom('.card-content-title');
+
+  assert.strictEqual(FIRST_RESTO_TITLE, FAV_RESTO_TITLE);
 });
 
 // SKENARIO UJI 3
-// Scenario('menampilkan satu galeri restoran yang tidak di-favoritkan', async ({I}) => {
-//   I.seeElement('.card a');
-//   const likedRestoTitle = await I.grabTextFrom('.card-content-title');
-//   I.click(likedRestoTitle);
+Scenario('menampilkan satu galeri restoran yang tidak di-favoritkan', async ({I}) => {
+  I.see(emptyFavoriteRestoText, '#fav-resto');
 
-  // URL: /resto/:id
-  // I.seeElement('#likeButton');
-  // I.click('#likeButton');
+  // URL: /
+  I.amOnPage('/');
+  I.seeElement('.card');
+
+  const FIRST_RESTO = locate('.card-content-title').first();
+  const FIRST_RESTO_TITLE = await I.grabTextFrom(FIRST_RESTO);
+  I.click(FIRST_RESTO);
+
+  // URL: /#/:detail
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  /* UNFAVORITE RESTO */ 
 
   // URL: /#/favorite
-  // I.amOnPage('/#/favorite');
-  // I.seeElement('#fav-resto');
-  // I.dontSeeElement('.card');
-  // I.dontSeeElement('.card-content-title');
-// });
+  I.amOnPage('/#/favorite');
+  I.seeElement('.card');
+
+  const UNFAV_RESTO_TITLE = await I.grabTextFrom('.card-content-title');
+
+  assert.strictEqual(FIRST_RESTO_TITLE, UNFAV_RESTO_TITLE);
+
+  I.click(UNFAV_RESTO_TITLE);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  // // URL: /#/favorite
+  I.amOnPage('/#/favorite');
+  I.seeElement('#fav-resto');
+
+  const UNFAV_RESTO = await I.grabTextFrom('#fav-resto');
+
+  // CEK APAKAH HALAMAN FAVORITE SUDAH KOSONG
+  assert.strictEqual(UNFAV_RESTO, emptyFavoriteRestoText);
+});
